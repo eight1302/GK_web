58486752b5eeb51ac7f4ac100b4b5ab50a8fcb63
eight1302 <380080496@qq.com>
2017-07-06 17:40:13 +0800

    Add files via upload

---
+++ styles/main.css.js
@@ -0,0 +1,33 @@
+// ============================
+// P1. Dependencies Styles
+// ============================
+import "angular-busy/dist/angular-busy.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css.map";
+import "font-awesome/css/font-awesome.min.css";
+import "sweetalert/dist/sweetalert.css";
+import "angular-tree-control/css/tree-control.css";
+import "angular-tree-control/css/tree-control-attribute.css";
+import {} from "../dependencies/ng-scrollable.min.css";
+
+// ============================
+// P2. Style Guideline
+// ============================
+import {} from "./purple/common.scss";
+import {} from "./purple/common.purple.scss";
+import {} from "./purple/auth.scss";
+import {} from "./purple/dashboard.scss";
+import {} from "./purple/enterprise.scss";
+import {} from "./purple/reports.scss";
+import {} from "./purple/system.scss";
+import {} from "./purple/policy.scss";
+import {} from "./red/common.red.scss";
+import {} from "./red/enterprise.scss";
+import {} from "./red/reports.scss";
+import {} from "./red/system.scss";
+import {} from "./red/policy.scss";
+import{}from "./violet/common.violet.scss";
+import {} from "./violet/enterprise.scss";
+import {} from "./violet/reports.scss";
+import {} from "./violet/system.scss";
+import {} from "./violet/policy.scss";
